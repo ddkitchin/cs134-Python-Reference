461cd2d138f151ec57fb04008c599ca9db5f80f3
ddkitchin <46247238+ddkitchin@users.noreply.github.com>
2025-07-14 18:13:58 -0500

    fix

---
+++ 2_abstraction_and_data_representation/grade.py
@@ -0,0 +1,14 @@
+# @copyright Deborah Kitchin
+
+def main() -> None:
+    # set real variable score to the user input
+    score = float(input("Enter score: "))
+
+    if score >= 59.5:
+        print("You passed")
+    else:
+        print("You failed")
+
+
+if __name__ == "__main__":
+    main()
