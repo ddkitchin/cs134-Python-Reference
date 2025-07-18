79fa65453f72b6a0134ab11f3460d9b8cc40038a
ddkitchin <46247238+ddkitchin@users.noreply.github.com>
2025-07-18 18:07:57 -0500

    added syntax_errors

---
+++ 3_introduction_to_python/syntax_errors.py
@@ -0,0 +1,40 @@
+# @copyright Deborah Kitchin
+
+def main() -> None:
+
+    print()
+    #Print(“JCCC”)
+
+    #print(“JCCC)
+
+    #pirnt(8+5)
+
+    #print ("JCCC")
+
+    #print{"JCCC")
+
+    # print("JCCC")
+
+    '''# Get the number
+    print("Enter a number, and I will double it!")
+    numToDouble = int(input("Your number? "))
+
+    # Display the number doubled
+    print()
+    print(numToDouble, "doubled is", numToDouble / 2)'''
+
+    '''# Get the number of pizza slices and people
+    numSlicess = int(input("How many slices of pizza are available? "))
+    numPeople = int(input("How many people are eating? "))
+
+    # Determine the number of slices for each person.
+    slicesPerPerson = numSlices / numPeople
+
+    # Display the number of slices per person
+    print()
+    print("Number of slices per person:", slicesPerPerson)'''
+
+    print('done')
+
+if __name__ == "__main__":
+    main()
